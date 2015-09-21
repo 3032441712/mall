56992c8fccea9fd28a4d44229869d55dede1f282
3032441712 <3032441712@qq.com>
2015-09-21 23:03:41 +0800

    TODO:添加jedis缓存 1 添加jedis缓存

--- src/main/java/com/dragon/mall/base/BaseService.java
+++ src/main/java/com/dragon/mall/base/BaseService.java
@@ -8,23 +8,42 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.log4j.Logger;
 
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 public class BaseService {
-    
+
     static Logger log = Logger.getLogger(BaseService.class);
-        
+
     private static SqlSessionFactory sqlSessionFactory = null;
-    
+
+    private static JedisPool jedisPool = null;
+
     public static SqlSessionFactory getSqlSessionFactory() {
         return sqlSessionFactory;
     }
 
+    public static JedisPool getJedisPool() {
+        return jedisPool;
+    }
+
     public static void initService() throws IOException {
-        if (sqlSessionFactory == null) {
+        if (BaseService.sqlSessionFactory == null) {
             String resource = "mybatis-config.xml";
             InputStream inputStream = Resources.getResourceAsStream(resource);
-            BaseService.sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            BaseService.sqlSessionFactory = new SqlSessionFactoryBuilder()
+                    .build(inputStream);
         }
+
+        if (BaseService.jedisPool == null) {
+            JedisPoolConfig config = new JedisPoolConfig();
+            config.setMaxTotal(100);
+            config.setMaxIdle(10);
+            config.setMaxWaitMillis(1000);
+
+            BaseService.jedisPool = new JedisPool(config, "localhost", 6379);
+        }
+
         log.info("初始化 Service");
-        log.info(BaseService.sqlSessionFactory);
     }
 }
--- src/main/java/com/dragon/mall/model/GoodsModel.java
+++ src/main/java/com/dragon/mall/model/GoodsModel.java
@@ -1,11 +1,28 @@
 package com.dragon.mall.model;
 
+import java.util.Map;
+
+import redis.clients.jedis.Jedis;
+
 import com.alibaba.fastjson.JSON;
+import com.dragon.mall.base.BaseService;
 import com.dragon.mall.service.GoodsService;
 
 public class GoodsModel {
     
     public static String info(Integer id) {
-        return JSON.toJSONString(GoodsService.getGoodsInfo(id));
+        Jedis jedis = BaseService.getJedisPool().getResource();
+        String content = jedis.get("goods:info:"+String.valueOf(id));
+        if (content == null) {
+            Map<String, Object> data = GoodsService.getGoodsInfo(id);
+            data.put("code", "1");
+            data.put("message", "success");
+            content = JSON.toJSONString(data);
+            jedis.set("goods:info:"+String.valueOf(id), content);
+            jedis.incr("goods:info:num:"+String.valueOf(id));
+        }
+        jedis.close();
+
+        return content;
     }
 }
